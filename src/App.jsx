c8492c704bdd8945035a2dbba2ac2118bf0251bc
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-13 17:49:49 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, commodi
-        repellat aliquid illum inventore nulla dolor? Nobis quisquam eos nulla
-        quas laborum, accusamus perferendis sequi minus excepturi, sit quo
-        quidem.
-      </p>
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   );
 };
 
