f45a0b674b05e992dfb7bc6a477920a6d6435018
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-13 17:59:38 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { appleImg, bagImg, searchImg } from '../utils';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="apple" width={14} height={18} />
+        <div className="">
+          {['Phones', 'Mackbooks', 'Tablets'].map((nav) => (
+            <div key={nav} className="">
+              {nav}
+            </div>
+          ))}
+        </div>
+        <div className="">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
