423998883b6d0d60c88495e7961aa3474912b0da
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-13 18:33:31 +0100

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,6 +26,11 @@ const Hero = () => {
       opacity: 1,
       delay: 1.5,
     });
+    gsap.to('#cta', {
+      opacity: 1,
+      y: -50,
+      delay: 1.5,
+    });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
@@ -44,6 +49,15 @@ const Hero = () => {
             <source src={videosrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        className="flex flex-col items-center opacity-0 translate-y-20"
+        id="cta"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
