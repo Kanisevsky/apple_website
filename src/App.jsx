44a76b6975f45a6aca45f051f8eb1acbe0bda955
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-13 17:30:45 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, commodi
+        repellat aliquid illum inventore nulla dolor? Nobis quisquam eos nulla
+        quas laborum, accusamus perferendis sequi minus excepturi, sit quo
+        quidem.
+      </p>
     </>
   );
 };
