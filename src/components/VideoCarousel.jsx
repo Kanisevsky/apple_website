4b3dbc0c0701c2e5e6ae604d16c32235dd0d2b30
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-14 19:03:21 +0100

    Highlights

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,16 @@
+import { hightlightsSlides } from '../utils';
 const VideoCarousel = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider" className="video-carousel_container">
+            Test
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
