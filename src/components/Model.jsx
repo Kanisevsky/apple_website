b07141f8cf595e8e22abb0e1509a36ae7d56df4e
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-16 15:33:33 +0100

    Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,6 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import ModelView from './ModelView';
 
 const Model = () => {
   useGSAP(() => {
@@ -16,7 +17,9 @@ const Model = () => {
           Take a closer look.
         </h1>
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
         </div>
       </div>
     </section>
