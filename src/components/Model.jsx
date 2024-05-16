f636dbc50a0585e7574ae38082917d28a44e2c5b
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-16 15:52:03 +0100

    Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,9 @@ import ModelView from './ModelView';
 import { useState, useRef } from 'react';
 import { yellowImg } from '../utils';
 import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { View } from '@react-three/drei';
+import { models } from '../constants';
 const Model = () => {
   const [size, setsize] = useState('small');
   const [model, setModel] = useState({
@@ -56,6 +59,35 @@ const Model = () => {
               item={model}
               size={size}
             />
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: 'hidden',
+              }}
+              eventSource={document.getElementById('root')}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  ></li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
