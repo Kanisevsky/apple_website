9c23948c866e57957b09c63de7cbf21793ad5a4a
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-16 20:50:56 +0100

    ModelView

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,32 +1,62 @@
-import { PerspectiveCamera, View } from '@react-three/drei';
-import { AmbientLight } from 'three';
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from '@react-three/drei';
+
+import * as THREE from 'three';
 import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
 import { Suspense } from 'react';
-const ModelView = (
+
+const ModelView = ({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
-  item
-) => {
+  item,
+}) => {
   return (
-    <div>
-      <View
-        index={index}
-        id={gsapType}
-        className={`w-full h-full absolute ${
-          index === 2 ? 'right-[-100%]' : ''
-        }`}
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
       >
-        {/* Ambient Light */}
-        <ambientLight intensity={0.3} />
-        <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-        <Lights />
-        <Suspense fallback={<div>Loading</div>} />
-      </View>
-    </div>
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
   );
 };
 
